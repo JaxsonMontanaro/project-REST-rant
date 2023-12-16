bd6fb06f002b73ecfabe911529d55372ff7416a4
JaxsonMontanaro <jaxsonmontanaro@gmail.com>
2023-12-15 19:19:56 -0800

    Adding Bootstrap grid to index page

--- veiws/places/index.jsx
+++ veiws/places/index.jsx
@@ -1,18 +1,20 @@
-function index (data) {
+  function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
-        <div>
+        <div className="col-sm-6">
           <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}/>
+          <img src={photo-1513618364580-fe1596762e8e.avif} alt={place.name}/>
         </div>
       )
     })
     return (
       <Def>
           <main>
-              <h1>PLACES INDEX PAGE</h1>
-              {placesFormatted}
+              <h1>Places to Rant or Rave About</h1>
+              <div className="row">
+              <img src={premium_photo-1664189121690-fd870956d9f4.avif} alt={place.name}/>
+              </div>
           </main>
       </Def>
-  )
+    )
   }
