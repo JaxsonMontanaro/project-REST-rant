b27446eb7ffb6cbba7d8fd3f542859b8e52ee1b4
JaxsonMontanaro <jaxsonmontanaro@gmail.com>
2023-12-17 22:00:56 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -2,45 +2,53 @@ const router = require('express').Router()
 const places = require('../models/places.js')
 
 app.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
-      
-      res.render('places/index', { places })
-  })
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'http://placekitten.com/250/250'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'http://placekitten.com/250/250'
+  }]
 
-  router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
+  res.render('places/index', { places })
+})
 
-  router.post('/', (req, res) => {
-    console.log(req.body)
-    router.post('/', (req, res) => {
-        console.log(req.body)
-        if (!req.body.pic) {
-          req.body.pic = 'http://placekitten.com/400/400'
-        }
-        if (!req.body.city) {
-          req.body.city = 'Anytown'
-        }
-        if (!req.body.state) {
-          req.body.state = 'USA'
-        }
-        places.push(req.body)
-        res.redirect('/places')
-      })
-      
-    res.send('POST /places')
-  })  
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show')
+  }
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+router.get('/:id', (req, res) => {
+  res.render('places/show', { place: places[id] })
+})
 
 module.exports = router
--- veiws/places/index.jsx
+++ veiws/places/index.jsx
@@ -1,12 +1,22 @@
   function index (data) {
-    let placesFormatted = data.places.map((place) => {
+    let placesFormatted = data.places.map((place, index) => {
       return (
         <div className="col-sm-6">
-          <h2>{place.name}</h2>
-          <img src={photo-1513618364580-fe1596762e8e.avif} alt={place.name}/>
+          <h2>
+            <a href={`/places/${index}`} >
+              {place.name}
+            </a>
+          </h2>
+          <p className="text-center">
+            {place.cuisines}
+          </p>
+          <img src={place.pic} alt={place.name} />
+          <p className="text-center">
+            Located in {place.city}, {place.state}
+          </p>
         </div>
       )
-    })
+    })    
     return (
       <Def>
           <main>
