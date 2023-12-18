8b214d45ffae6cd0b15da80387d64e19d3f6152a
JaxsonMontanaro <jaxsonmontanaro@gmail.com>
2023-12-17 22:22:25 -0800

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -19,16 +19,16 @@ app.get('/', (req, res) => {
   res.render('places/index', { places })
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-    res.render('error404')
+      res.render('error404')
   }
   else if (!places[id]) {
-    res.render('error404')
+      res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id], id })
+    res.render('places/edit', { place: places[id] })
   }
 })
 
