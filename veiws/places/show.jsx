9d4a0790499b0ac02403b3444a0526655ce861eb
JaxsonMontanaro <jaxsonmontanaro@gmail.com>
2023-12-17 22:09:15 -0800

    Delete functionality implemented

--- veiws/places/show.jsx
+++ veiws/places/show.jsx
@@ -1,14 +1,23 @@
 const React = require('react')
-const Def = require('../default')
+const Def = require('../default') 
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <h1>{ data.place.rating }</h1>
+            <h1>{ data.place.description }</h1>
+            <h1>{ data.place.comment }</h1>
           </main>
         </Def>
     )
 }
+
+<form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+  <button type="submit" className="btn btn-danger">
+    Delete
+  </button>
+</form> 
 
 module.exports = show
