1ae88f0ff4926ba5668a20ce3cda9ff960ccdc8e
JaxsonMontanaro <jaxsonmontanaro@gmail.com>
2023-12-26 12:45:47 -0800

    rounding rating

--- veiws/places/show.jsx
+++ veiws/places/show.jsx
@@ -1,50 +1,41 @@
 const React = require("react");
 const Def = require("../default");
 
-function show (data) {
-    let comments = (
-      <h3 className="inactive">
-        No comments yet!
-      </h3>
-    )
-    if (data.place.comments.length) {
-      comments = data.place.comments.map(c => {
-        return (
-          <div className="border">
-            <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
-            <h4>{c.content}</h4>
-            <h3>
-              <stong>- {c.author}</stong>
-            </h3>
-            <h4>Rating: {c.stars}</h4>
-          </div>
-        )
-      })
-    }
+function show(data) {
+  let comments = <h3 className="inactive">No comments yet!</h3>;
 
-  return (
-    <Def>
-      <div className="row">
-        <div className="col-sm-6">
-          <img src={data.place.pic} alt={data.place.name} />
-          <h3>
-            Located in {data.place.city}, {data.place.state}
-          </h3>
+  let rating = <h3 className="inactive">Not yet rated</h3>;
+  if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars;
+    }, 0);
+    let averageRating = sumRatings / data.place.comments.length;
+    rating = <h3>{averageRating} stars</h3>;
+
+    return (
+      <Def>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
+          </div>
         </div>
-        <div className="col-sm-6">
-          <h2>Description</h2>
-          <h3>{data.place.showEstablished()}</h3>
-          <h4>Serving {data.place.cuisines}</h4>
-        </div>
-      </div>
-      <main>
-        <h1>{data.place.name}</h1>
-        <h1>{data.place.rating}</h1>
-        <h1>{data.place.description}</h1>
-        <h1>{data.place.comment}</h1>
-      </main>
-    </Def>
-  );
+        <main>
+          <h1>{data.place.name}</h1>
+          <h1>{data.place.rating}</h1>
+          <h1>{data.place.description}</h1>
+          <h1>{data.place.comment}</h1>
+        </main>
+      </Def>
+    );
+  }
 }
 
 <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
